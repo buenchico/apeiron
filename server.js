81ba99cf79f4afed32e3e97c0913ba334c1ccba8
beunchico <hammer_ortiz@hotmail.com>
2025-06-06 17:07:00 +0100

    adding ejs partials to node

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files
+// Set up EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Ensure your views are stored in the 'views' folder
+
+// Serve static files (CSS, images, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define a route that renders an EJS template
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index', { title: 'Welcome to My Website' });
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
