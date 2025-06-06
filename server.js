44211932ec1e550dc324eaaeaefea58a91eff71b
beunchico <hammer_ortiz@hotmail.com>
2025-06-06 17:04:08 +0100

    adding express to node

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    let filePath = req.url === '/' ? 'public/index.html' : `public${req.url}`;
-    filePath = path.join(__dirname, filePath);
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // Determine content type
-    let contentType = 'text/html';
-    if (req.url.endsWith('.css')) contentType = 'text/css';
-    else if (req.url.endsWith('.js')) contentType = 'application/javascript';
-    else if (req.url.endsWith('.png')) contentType = 'image/png';
-    else if (req.url.endsWith('.jpg')) contentType = 'image/jpeg';
-
-    // Read and serve the file
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('404 Not Found');
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(data);
-        }
-    });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
